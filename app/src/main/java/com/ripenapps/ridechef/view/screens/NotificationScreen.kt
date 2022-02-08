084b493abd796378466df5fce71a1e65e9525979
Yaman Aswal <yaman.aswal@ripenapps.in>
2022-02-08 17:12:57 +0530

    Updated Rest Screen

---
+++ app/src/main/java/com/ripenapps/ridechef/view/screens/NotificationScreen.kt
@@ -0,0 +1,20 @@
+package com.ripenapps.ridechef.view.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ripenapps.ridechef.R
+
+class NotificationScreen : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notification_screen, container, false)
+    }
+
+}
--- app/src/main/java/com/ripenapps/ridechef/view/screens/SplashScreen.kt
+++ app/src/main/java/com/ripenapps/ridechef/view/screens/SplashScreen.kt
@@ -1,11 +1,13 @@
 package com.ripenapps.ridechef.view.screens
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -23,18 +25,30 @@ class SplashScreen : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_splash_screen, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_splash_screen, container, false)
         splashTimer()
         return binding.root
     }
 
     private fun splashTimer() {
         val splashViewModel = ViewModelProvider(this)[SplashViewModel::class.java]
-        splashViewModel.splashStatus.observe(viewLifecycleOwner,{ status ->
-            if(status){
-                this.findNavController().navigate(SplashScreenDirections.actionSplashScreenToAllowLocationScreen())
+        splashViewModel.splashStatus.observe(viewLifecycleOwner, { status ->
+            if (status) {
+                this.findNavController()
+                    .navigate(SplashScreenDirections.actionSplashScreenToAllowLocationScreen())
             }
         })
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
     }
 
 }
